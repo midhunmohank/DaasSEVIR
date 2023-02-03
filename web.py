64114d739ea16b5a3fbb59f77d121e02f864ccac
midhunmohank <mohan.ku@northeastern.edu>
2023-02-02 22:45:52 -0500

    lfs migration for py files
    Former-commit-id: 5ee702594d5ceac6a9a465c6bac3480454582089 [formerly d90eed79131fb36fd5f2ec048fdd76ced8a9db7b]
    Former-commit-id: 4c9369c083f3fc6b502f72bd0674268ef62c56b4

--- web.py
+++ web.py
@@ -1,20 +1,3 @@
-
-import streamlit as st
-import pandas as pd
-import folium
-
-# Load the latitude and longitude data into a pandas dataframe
-data = {'ICAO': ['KABR', 'KABX', 'KAKQ', 'KAMA'],
-        'LAT': [45.455833, 35.149722, 36.98405, 35.233333],
-        'LON': [-98.413333, -106.82388, -77.007361, -101.70927]}
-df = pd.DataFrame(data)
-
-st.title("Map Visualization using Streamlit")
-
-# Create the map using folium and display in Streamlit
-map = folium.Map(location=[df['LAT'].mean(), df['LON'].mean()], zoom_start=5)
-
-for lat, lon, name in zip(df['LAT'], df['LON'], df['ICAO']):
-    folium.Marker(location=[lat, lon], popup=name).add_to(map)
-
-st.write(map)
+version https://git-lfs.github.com/spec/v1
+oid sha256:c47f9f6c134a8858111db25a8c0ea9428dd005bde7e287c81dcb602d1d506a00
+size 641
