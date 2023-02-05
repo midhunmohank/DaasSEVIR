a904872ba90005181d6e83ac5cdd508328828381
midhunmohank <mohan.ku@northeastern.edu>
2023-02-05 17:59:43 -0500

    test ui query updated

--- populating_ui_from_db_testing/query_test_ui.py
+++ populating_ui_from_db_testing/query_test_ui.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d06ebcdadc36f2827bc8b1361e4c0202e955057c086661121b3a27ae54b1212
-size 1416
+oid sha256:01b52d4420a07654736dab8a7ef5b49aa62fb63d06a750e0e09fc296c7b07602
+size 1710
