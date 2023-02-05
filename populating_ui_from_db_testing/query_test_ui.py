1719becc4cd6d6ea19447a51692122cd5dae44b2
vikash-singh-prac <singh.vikas@northeastern.edu>
2023-02-05 18:35:14 -0500

    Updated functionality

--- populating_ui_from_db_testing/query_test_ui.py
+++ populating_ui_from_db_testing/query_test_ui.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f497f11e20bdaa2ad8f924fc0cd86d411d8b0810d8d61061210af2b8d232009
-size 2195
+oid sha256:7bc8cff75e5601e3b56e9363d6495605982f7a9205d50d328953db4cc871c164
+size 2171
