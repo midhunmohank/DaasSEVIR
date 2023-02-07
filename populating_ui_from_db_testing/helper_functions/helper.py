daaf508c6bc6fbe440b49b5385bad5303e1b498f
snehil.aryan <aryan.s@northeastern.edu>
2023-02-06 21:23:23 -0500

    Added Logging feature for goes url generate

--- populating_ui_from_db_testing/helper_functions/helper.py
+++ populating_ui_from_db_testing/helper_functions/helper.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49eeed572e93491967bd28acc7d9717dce2bf9666897dae3eb5628bb3f47d7fc
-size 2620
+oid sha256:e5d876b772ec59cac7c2ca7801c31b7aa07d0fc71b0be19748c0d6012d2ee7aa
+size 2736
--- populating_ui_from_db_testing/pages/GOES.py
+++ populating_ui_from_db_testing/pages/GOES.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c1dd88ad4c33f95c463e01aa71497bba6855aabf29bcebbfc1b3218d97ca99b
-size 3478
+oid sha256:7f7194543ada0198e825d4252430f0d4ad8bbcad9bfbc270cd3956830a2d1ac6
+size 3658
