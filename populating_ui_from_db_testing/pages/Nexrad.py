890af26eb40c2dcd949429d3af76aeb9e594ea14
Midhun Mohan <k.midhun@outlook.com>
2023-02-08 18:57:57 -0500

    logic fixes on nexrad form population db

--- populating_ui_from_db_testing/pages/Nexrad.py
+++ populating_ui_from_db_testing/pages/Nexrad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6859fa54a9bde0ecacc97fdc58baf8c317d4210670fdc20d43967e8af13257b1
-size 9585
+oid sha256:0b2b7c8653ddc3ba5265c7a748d35c90f392470784284c1f36a02c35691e1b2d
+size 10160
