0379d5111550c04b7071adf97257e31359a2b6cc
snehil.aryan <aryan.s@northeastern.edu>
2023-02-08 19:35:54 -0500

    Completed NEX Rad URL code and fixed bug in copy to s3

--- populating_ui_from_db_testing/helper_functions/helper.py
+++ populating_ui_from_db_testing/helper_functions/helper.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5d876b772ec59cac7c2ca7801c31b7aa07d0fc71b0be19748c0d6012d2ee7aa
-size 2736
+oid sha256:fee8817491cbd94418eb0b82659c7a0ffd4de1a7fa043702f88def8feabfb092
+size 2738
--- populating_ui_from_db_testing/pages/Nexrad.py
+++ populating_ui_from_db_testing/pages/Nexrad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:096b4790faf92b41552cbcbebe8622d4efe1e47f79cebe2e88aaffc9ea349cb5
-size 12333
+oid sha256:615aa7c3f5b5d859ce80f5d236a9b63c522cf49c831df01322ecaca80eca591f
+size 12944
