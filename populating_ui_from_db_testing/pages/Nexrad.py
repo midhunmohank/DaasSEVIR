db00477d6fefe78fdd0ad672c8fd5366339c44ba
snehil.aryan <aryan.s@northeastern.edu>
2023-02-08 19:18:08 -0500

    Added Midhun's NEXRAD UI CODE

--- populating_ui_from_db_testing/pages/Nexrad.py
+++ populating_ui_from_db_testing/pages/Nexrad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60fbb5fc517d46faf70a01cb77e77ed188d9ff4dc9da75d63ad1b82fc299b050
-size 9619
+oid sha256:096b4790faf92b41552cbcbebe8622d4efe1e47f79cebe2e88aaffc9ea349cb5
+size 12333
