71a653903e3ec6d36554f06808ebfd5d4d8cae4f
Midhun Mohan <k.midhun@outlook.com>
2023-02-10 12:06:58 -0500

    goes validations update

--- pages/GOES.py
+++ pages/GOES.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c95e2f9b22008f4473682f2239d87c7b39d69f5adcf760ab6fa35f4d9fd3fed
-size 3820
+oid sha256:c8a5b5534781bfe3ae07f2ab066eaa4f8758a25596a9a3d12192ddb73afb333e
+size 4413
